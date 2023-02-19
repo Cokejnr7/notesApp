c7b62de17fad8c0ab1ae8a82a18867e444c5cab6
Kolawole Daniel <kolawolecoke@gmail.com>
2023-02-19 07:17:06 +0100

    first commit

--- backend/api/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-
-class ApiTest(TestCase):
-
-    def test_note(self):
-        pass
--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import NoteViewset
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register("notes",NoteViewset,basename='notes')
-
-urlpatterns = router.urls
-
